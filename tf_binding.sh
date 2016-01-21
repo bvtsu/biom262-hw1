68531e1a6a4905f9a683ed6f05b42d4fd909dc81
acarreno13 <acarreno13@gmail.com>
2016-01-21 12:10:07 -0800

    alberto1-3

--- tf_binding.sh
+++ tf_binding.sh
@@ -10,7 +10,40 @@
 #PBS -m abe
 
 cd ~/code/biom262-2016/weeks/week01/data
-
+%%bash --out exercise1
+# YOUR CODE HERE
+grep 'NFKB' tf.bed > tf.nfkb.bed
+wc -l tf.nfkb.bed
+echo '--- First 10 lines ---'
+head tf.nfkb.bed
+echo '--- Random 10 lines ---'
+awk -v seed=907 'BEGIN{srand(seed);}{print $0}'  tf.nfkb.bed | head
+echo '--- Last 10 lines ---'
+tail tf.nfkb.bed
+print(exercise1)
+%%bash --out exercise2
+# YOUR CODE HERE
+cat gencode.v19.annotation.chr22.gtf | awk '$3 ~ /transcript/ {print}' > gencode.v19.annotation.chr22.transcript.gtf
+wc -l gencode.v19.annotation.chr22.transcript.gtf
+echo '--- First 10 lines ---'
+head gencode.v19.annotation.chr22.transcript.gtf
+echo '--- Random 10 lines ---'
+awk -v seed=907 'BEGIN{srand(seed);}{print $0}' gencode.v19.annotation.chr22.transcript.gtf | head
+echo '--- Last 10 lines ---'
+tail gencode.v19.annotation.chr22.transcript.gtf
+print(exercise2)
+%%bash --out exercise3
+# YOUR CODE HERE
+module load biotools
+bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome  -l 2000 -r 0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
+wc -l gencode.v19.annotation.chr22.transcript.promoter.gtf
+echo '--- First 10 lines ---'
+head gencode.v19.annotation.chr22.transcript.promoter.gtf
+echo '--- Random 10 lines ---'
+awk -v seed=907 'BEGIN{srand(seed);}{print rand()" "$0}' gencode.v19.annotation.chr22.transcript.promoter.gtf | head
+echo '--- Last 10 lines ---'
+tail gencode.v19.annotation.chr22.transcript.promoter.gtf
+print(exercise3)
 
 
 
@@ -47,3 +80,4 @@ tail gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
 print(exercise5)
 
 echo "Hello I am a message in standard out (stdout)"
+echo "Hello I am a message in standard error (stderr) >&2"
